f6b4f8ab4267e49b2c3fafe8bef5a6ade13edbb6
rakshitha <allurirakshithaa@gmail.com>
2024-11-03 19:58:24 +0530

    solution

--- src/components/ChatLayout.js
+++ src/components/ChatLayout.js
@@ -5,8 +5,6 @@ import io from 'socket.io-client';
 import ChatRoom from './ChatRoom';
 import RoomList from './RoomList';
 import CreateRoom from './CreateRoom';
-import PrivateMessageList from './PrivateMessageList';
-import PrivateMessageThread from './PrivateMessageThread';
 
 const ChatLayout = () => {
   const { token, username } = useAuth();
@@ -15,35 +13,24 @@ const ChatLayout = () => {
   const [selectedPrivateConversation, setSelectedPrivateConversation] = useState(null);
   const [rooms, setRooms] = useState([]);
 
-  // ... (existing code)
-
   return (
     <div className="flex h-screen">
       <div className="w-1/4 bg-gray-800 p-4">
-        <CreateRoom onRoomCreated={fetchRooms} />
+       
         <RoomList
           rooms={rooms}
           selectedRoom={selectedRoom}
           onRoomSelect={setSelectedRoom}
         />
-        <PrivateMessageList
-          onPrivateConversationSelect={setSelectedPrivateConversation}
-        />
-      </div>
+        
       <div className="flex-1">
-        {selectedRoom ? (
+       
           <ChatRoom
             room={selectedRoom}
             socket={socket}
             username={username}
           />
-        ) : selectedPrivateConversation ? (
-          <PrivateMessageThread recipient={selectedPrivateConversation} />
-        ) : (
-          <div className="h-full flex items-center justify-center text-gray-500">
-            Select a room or a private conversation to start chatting
-          </div>
-        )}
+      </div>
       </div>
     </div>
   );
